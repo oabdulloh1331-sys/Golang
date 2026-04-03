af29cd596d672eab2df4d4b646a6f32d9642f737
oabdulloh1331-sys <o.abdulloh.1331@gmail.com>
2026-04-03 16:48:38 +0500

    update

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
 import "fmt"
+var name string 
+var age int8
 
 func main() {
-    fmt.Println("hi cat")
+    fmt.Println("What is your name?")
+    fmt.Scanf("%s", &name)
+    fmt.Println("What is your age?")
+    fmt.Scanf("%d", &age)
+    fmt.Println("Hello, " + name)
+    fmt.Println("You are " + fmt.Sprint(age) + " years old.")
 }
+
