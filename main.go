2b7b353f2772ea7757ed76c45e53a50680252f7f
oabdulloh1331-sys <o.abdulloh.1331@gmail.com>
2026-04-04 22:04:09 +0500

    update

--- main.go
+++ main.go
@@ -2,14 +2,21 @@ package main
 
 import "fmt"
 
-var password string 
-
 func main() {
-    fmt.Println("Enter password:")
-    fmt.Scanf("%s", &password)
-    if password == "12346" {
-        fmt.Println("Password correct.")
+    var sum int
+    var age int
+
+    fmt.Println("Enter the total sum:")
+    fmt.Scanf("%d", &sum)
+
+    fmt.Println("Enter your age:")
+    fmt.Scanf("%d", &age)
+
+    if sum > 5000 || age > 60 {
+        fmt.Println("You have 20% discount!")
+    } else if sum > 2000 && age < 18 {
+        fmt.Println("You have 10% discount!")
     } else {
-        fmt.Println("Password incorrect.")
+        fmt.Println("No discount today.")
     }
 }
