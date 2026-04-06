704d1aed44003a5cb61f6316d56dc557f6c0370e
oabdulloh1331-sys <o.abdulloh.1331@gmail.com>
2026-04-06 18:38:39 +0500

    finished if-else tasks

--- main.go
+++ main.go
@@ -3,20 +3,13 @@ package main
 import "fmt"
 
 func main() {
-    var sum int
-    var age int
+   password := ""
+for password != "1234" {
+    fmt.Println("Vvedite parol:")
+    fmt.Scan(&password)
+}
+fmt.Println("Dostup otkrit!")
 
-    fmt.Println("Enter the total sum:")
-    fmt.Scanf("%d", &sum)
 
-    fmt.Println("Enter your age:")
-    fmt.Scanf("%d", &age)
 
-    if sum > 5000 || age > 60 {
-        fmt.Println("You have 20% discount!")
-    } else if sum > 2000 && age < 18 {
-        fmt.Println("You have 10% discount!")
-    } else {
-        fmt.Println("No discount today.")
-    }
 }
